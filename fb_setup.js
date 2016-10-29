f74a439fab70cee46073f7df4e1283a315ff3f42
matyapav <matyapav@fel.cvut.cz>
2016-10-29 17:32:56 +0200

    Test FB loginu #10

--- fb_setup.js
+++ fb_setup.js
@@ -92,7 +92,7 @@ window.fbAsyncInit = function() {
 // successful.  See statusChangeCallback() for when this call is made.
 function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me?fields=name,email', function(response) {
+    FB.api('/me',{ fields: 'name, email' }, function(response) {
         console.log('Successful login for: ' + response.name + " "+response.email);
 
         //httpRequest("GET", "http://localhost/api/)
@@ -109,7 +109,7 @@ function fblogin()
         } else {
             console.log('User cancelled login or did not fully authorize.');
         }
-    }, { scope: 'email,name' });
+    }, { scope: 'email' });
 
 }
 
