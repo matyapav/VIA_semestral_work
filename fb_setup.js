619710bce7bff39c45c9692c8f744fa7627e4eab
matyapav <matyapav@fel.cvut.cz>
2016-11-03 16:59:37 +0100

    Login logout test #4.5

--- fb_setup.js
+++ fb_setup.js
@@ -1,5 +1,23 @@
 var isLoggedIn = false;
 
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+        // Logged into your app and Facebook.\
+
+    } else if (response.status === 'not_authorized') {
+        // The person is logged into Facebook, but not your app.
+    } else {
+        // The person is not logged into Facebook, so we're not sure if
+        // they are logged into this app or not.
+    }
+}
+
 function subscribeToEvents() {
     FB.Event.subscribe('auth.logout', logout_event);
 }
@@ -67,7 +85,6 @@ function loginUserIntoApplication() {
             }
             console.log(localStorage.getItem("id"));
             performLoginActions();
-
         });
 
     });
@@ -91,7 +108,6 @@ function fblogin() {
             if(!localStorage.getItem("id")){
                 loginUserIntoApplication();
             }
-            performLoginActions();
         } else {
             console.log('User cancelled login or did not fully authorize.');
         }
@@ -118,7 +134,6 @@ function performLoginActions() {
     makeCorsRequest("GET", "https://ivebeenthereapi-matyapav.rhcloud.com/users/"+userId, null, function (responseText) {
         document.getElementById('status').innerHTML = 'Přihlášen jako, ' + JSON.parse(responseText).name + '!';
     });
-
 }
 
 var logout_event = function(response) {
