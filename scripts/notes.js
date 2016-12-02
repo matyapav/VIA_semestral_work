909620a151fed8e9f5918f04626b2f55f882c4e5
matyapav <matyapav@fel.cvut.cz>
2016-12-02 14:51:35 +0100

    add notes #1.4 url corrections

--- scripts/notes.js
+++ scripts/notes.js
@@ -12,7 +12,7 @@ function prepareNoteForm(place_name) {
                     "<input id='note_name' type='text'><br>" +
                     "<label for='note_content'>Obsah</label>" +
                     "<input id='note_content' type='text'><br>" +
-                    "<button id='add-note-submit-btn' onclick=''>Přidat</button>";
+                    "<button id='add-note-submit-btn'>Přidat</button>";
                 document.getElementById("add-note-submit-btn").addEventListener("click", function () {
                     var noteObj = {name: document.getElementById("note_name").value, content: document.getElementById("note_content").value}
                     console.log(noteObj);
@@ -25,7 +25,7 @@ function prepareNoteForm(place_name) {
 function addNoteToPlace(place_id, note) {
     var data = "&name="+note.name+"&content="+note.content;
     var user_id = localStorage.getItem("id");
-    var url = "https://ivebeenthereapi-matyapav.rhcloud.com/notes/place/"+place_id+"/user/"+user_id;
+    var url = "https://ivebeenthereapi-matyapav.rhcloud.com/notesForPlace/"+place_id+"/user/"+user_id;
     makeCorsRequest("POST", url, data, function (response) {
         console.log(response);
     })
@@ -41,7 +41,7 @@ function removeNote(note_id) {
 
 function getNotesForPlace(place_id) {
     var user_id = localStorage.getItem("id");
-    var url = "https://ivebeenthereapi-matyapav.rhcloud.com/notes/place/"+place_id+"/user/"+user_id;
+    var url = "https://ivebeenthereapi-matyapav.rhcloud.com/notesForPlace/"+place_id+"/user/"+user_id;
     makeCorsRequest("GET", url, null, function (response) {
         console.log(response);
         //TODO show notes
